f4dc0f571b226ef85752b885a8229ab26a8f9e8f
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-08 21:36:18 -0300

    client component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
