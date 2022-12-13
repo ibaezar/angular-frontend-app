47c9ef73387f7dc9dc52557293b3af30e4977490
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-13 14:54:36 -0300

    Sweetalert2 added

--- src/app/client/form.component.ts
+++ src/app/client/form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Client } from './client';
 import { ClientService } from './client.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -20,7 +21,10 @@ export class FormComponent {
 
   public create(): void {
     this.clientService.create(this.client).subscribe(
-      response => this.router.navigate(['/clientes'])
+      response => {
+        this.router.navigate(['/clientes'])
+        Swal.fire('Cliente creado', `Cliente ${this.client.name} creado con éxito`, 'success');
+      }
     );
   }
 
