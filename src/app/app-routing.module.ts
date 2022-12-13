79fdd67b1e5c06afd6ab5396eba9a8a4fb8fc90e
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-13 13:34:17 -0300

    form component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './client/client.component';
+import { FormComponent } from './client/form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full' },
-  { path: 'clientes', component: ClientComponent}
+  { path: 'clientes', component: ClientComponent},
+  { path: 'clientes/form', component: FormComponent }
 ];
 
 @NgModule({
--- src/app/client/form.component.ts
+++ src/app/client/form.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Client } from './client';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
 })
 export class FormComponent {
+
+  public title: string = "Crear cliente";
+  public client: Client = new Client();
+
+  public create(): void {
+    console.log("Data Client", this.client);
+  }
 
 }
